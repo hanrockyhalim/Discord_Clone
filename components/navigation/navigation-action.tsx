f5dbafe2d77d425ce4f40fe440eedf82837d9938
Hanrocky <hanrocky.halim@gmail.com>
2023-09-23 23:40:19 +0700

    Added action tooltip

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -5,7 +5,7 @@ import { Plus } from "lucide-react";
 export const NavigationAction = () => {
   return (
     <div>
-      <button className="grou flex items-center">
+      <button className="group flex items-center">
         <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] transition-all overflow-hidden group-hover:rounded-[16px] items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
           <Plus className="group-hover:text-white transition text-emerald-500" size={25} />
         </div>
